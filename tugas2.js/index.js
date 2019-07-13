c89fd74674481a313d9fd9309cb30e438d0b85e5
Engkos <engkoskostaman97@gmail.com>
2019-07-14 02:18:56 +0700

    Tugas 2 javascript
    tugas ini kami kumpulkan untuk niomic

---
+++ tugas2.js/index.js
@@ -0,0 +1,17 @@
+var positife = 20 ;
+var ganda='ganda'
+var tinggi =false;
+var cm = 185;
+if (cm>170)
+tinggi = true;
+tinggi = false;
+var  tambah
+var angka =[3,4,5,6,7];
+
+
+console.log (angka[3]);
+
+console.log(tambah)
+console.log(tinggi);
+console.log(ganda);
+console.log(positife);
